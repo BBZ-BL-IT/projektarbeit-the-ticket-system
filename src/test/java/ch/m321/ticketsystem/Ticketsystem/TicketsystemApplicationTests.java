6a1aee76da109eb430a43bf4f77108892f5196c1
robinb19 <111273003+robinb19@users.noreply.github.com>
2024-09-23 08:31:18 +0200

    init

---
+++ src/test/java/ch/m321/ticketsystem/Ticketsystem/TicketsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.m321.ticketsystem.Ticketsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
