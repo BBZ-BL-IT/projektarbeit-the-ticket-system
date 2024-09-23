977e71744cf80624dff2b4051e0d94bdc10e6cb8
LucaGGG <gass@byron.ch>
2024-09-23 11:28:57 +0200

    feat Ticketliste erstellt

--- Frontend/ticketing-frontend/src/app/ticket-list/ticket-list.component.ts
+++ Frontend/ticketing-frontend/src/app/ticket-list/ticket-list.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { MatIcon } from '@angular/material/icon';
+import { Ticket } from '../models/ticket';
+import { NgFor } from '@angular/common';
+import { MatCard, MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-ticket-list',
   standalone: true,
-  imports: [MatIcon],
+  imports: [MatIcon, NgFor, MatCardModule],
   templateUrl: './ticket-list.component.html',
   styleUrl: './ticket-list.component.scss'
 })
 export class TicketListComponent {
+
+  public tickets: Ticket[] = [{id: "1", title: "Ticket1", description: "sfgsdfgdsfg"}, {id: "2", title: "Ticket2", description: "dssfgsfg"}]
 
 }
