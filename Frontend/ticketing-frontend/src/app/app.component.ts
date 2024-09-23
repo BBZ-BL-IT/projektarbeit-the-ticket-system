df5b1d2bc37409e1b247cd664fd5eddfcc109099
LucaGGG <gass@byron.ch>
2024-09-23 09:27:18 +0200

    feat Startseite mit Toolbar erstellt

--- Frontend/ticketing-frontend/src/app/app.component.ts
+++ Frontend/ticketing-frontend/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TicketListComponent } from './ticket-list/ticket-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TicketListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ Frontend/ticketing-frontend/src/app/ticket-list/ticket-list.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { MatIcon } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-ticket-list',
+  standalone: true,
+  imports: [MatIcon],
+  templateUrl: './ticket-list.component.html',
+  styleUrl: './ticket-list.component.scss'
+})
+export class TicketListComponent {
+
+}
