7daf632c8f8f022f3b8f6c966ec17b73ab6dc40f
robinb19 <111273003+robinb19@users.noreply.github.com>
2024-10-08 11:21:42 +0200

    Refactoring

--- src/test/java/ch/m321/ticketsystem/TicketsystemApplicationTests.java
+++ src/test/java/ch/m321/ticketsystem/TicketsystemApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class TicketsystemApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
