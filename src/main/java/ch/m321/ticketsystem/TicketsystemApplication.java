08eed8455c4e9944edb091887df8db46bd7d3b63
robinb19 <111273003+robinb19@users.noreply.github.com>
2024-10-08 11:27:06 +0200

    Database update by restart delete unused classes

--- src/main/java/ch/m321/ticketsystem/TicketsystemApplication.java
+++ src/main/java/ch/m321/ticketsystem/TicketsystemApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TicketsystemApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TicketsystemApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TicketsystemApplication.class, args);
+    }
 
 }
