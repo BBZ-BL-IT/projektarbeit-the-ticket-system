ca652122031332b91f5d92ca173c1560eda3831b
jamie <jamie.niederhauser@bbzbl-it.ch>
2024-10-09 16:43:25 +0200

    deleted annotation

--- src/main/java/ch/m321/ticketsystem/TicketsystemApplication.java
+++ src/main/java/ch/m321/ticketsystem/TicketsystemApplication.java
@@ -2,10 +2,8 @@ package ch.m321.ticketsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 
 @SpringBootApplication
-@EnableRabbit
 public class TicketsystemApplication {
 
     public static void main(String[] args) {
